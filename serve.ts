4e3ca5f0c27327e13fe93d9f763f4ca679468e98
GitHub <noreply@github.com>
2024-03-25 09:14:03 +0000

    chore(deps): update deno dependencies

--- serve.ts
+++ serve.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.123.0/http/server.ts";
-import * as path from "https://deno.land/std@0.123.0/path/mod.ts";
-import { contentType } from "https://deno.land/x/media_types@v2.12.1/mod.ts";
+import { serve } from "https://deno.land/std@0.220.1/http/server.ts";
+import * as path from "https://deno.land/std@0.220.1/path/mod.ts";
+import { contentType } from "https://deno.land/x/media_types@deprecated/mod.ts";
 
 import { listeners } from "./listener.ts";
 
